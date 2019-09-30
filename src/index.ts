51217645c5bf9c5ed7369fb56fdce33e29fb2143
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-09-30 23:27:13 +0300

    build: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export interface output {
+export interface Output {
   repeated: string;
   count: number;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,60 +2,60 @@ import unrepeat from '../src';
 
 describe('unrepeat', () => {
   it('should work with single character string', () => {
-    let char = 'a';
-    let repCount = 1;
+    const char = 'a';
+    const repCount = 1;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
   });
 
   it('should work with single character repeats', () => {
-    let char = 'a';
-    let repCount = 50;
+    const char = 'a';
+    const repCount = 50;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
   });
 
   it('should work with even number of characters', () => {
-    let char = 'ab';
-    let repCount = 5;
+    const char = 'ab';
+    const repCount = 5;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
   });
 
   it('should work with no repeats', () => {
-    let char = 'test';
-    let repCount = 1;
+    const char = 'test';
+    const repCount = 1;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
   });
 
   it('should work with big number of repeats', () => {
-    let char = 'test';
-    let repCount = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23;
+    const char = 'test';
+    const repCount = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
   });
 
   it('should work with empty strings', () => {
-    let char = '';
-    let repCount = 1;
+    const char = '';
+    const repCount = 1;
 
-    var result = unrepeat(char.repeat(repCount));
+    const result = unrepeat(char.repeat(repCount));
 
     expect(result.repeated).toEqual(char);
     expect(result.count).toEqual(repCount);
