c39fec9b6fd8b0179071d04748f0c5cec22168b6
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-09-30 22:56:04 +0300

    feat: implement new algorithm

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,48 @@ export interface output {
  * @param string - The string that will be searched for repeats.
  * @returns Object containing the repeated string and the repeat count.
  */
-export default function unrepeat(string: any): output {
+export default function unrepeat(text: string): output {
+  // start from the minimum prime number and iterate till the text length
+  for (let i = 2; i <= text.length; i++) {
+    // check if text can be divided by i without remainder
+    if (text.length % i === 0) {
+      // possible repeated string and its length
+      const repLength = text.length / i;
+      const repeated = text.substr(0, repLength);
+
+      // set this flag false if it is not repeated on every step
+      let isRepeated = true;
+
+      // start scanning the original string
+      for (let j = 1; j < i; j++) {
+        if (repeated !== text.substr(j * repLength, repLength)) {
+          isRepeated = false;
+          break;
+        }
+      }
+
+      if (isRepeated) {
+        // repeated is the text itself
+        if (i === text.length) {
+          return {
+            repeated,
+            count: i,
+          };
+        } else {
+          // repeated is the part of the text, call unrepeat recursively
+          const res = unrepeat(repeated);
+          return {
+            repeated: res.repeated,
+            count: text.length / res.repeated.length,
+          };
+        }
+      }
+    }
+  }
+
+  // if no repeats found, then return the input itself
   return {
-    repeated: "a",
-    count: 5
+    repeated: text,
+    count: 1,
   };
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,59 @@
-import unrepeat from "../src";
+import unrepeat from '../src';
 
-describe("unrepeat", () => {
-  it("should work with single character string", () => {
-    let char = "a";
+describe('unrepeat', () => {
+  it('should work with single character string', () => {
+    let char = 'a';
+    let repCount = 1;
+
+    var result = unrepeat(char.repeat(repCount));
+
+    expect(result.repeated).toEqual(char);
+    expect(result.count).toEqual(repCount);
+  });
+
+  it('should work with single character repeats', () => {
+    let char = 'a';
+    let repCount = 50;
+
+    var result = unrepeat(char.repeat(repCount));
+
+    expect(result.repeated).toEqual(char);
+    expect(result.count).toEqual(repCount);
+  });
+
+  it('should work with even number of characters', () => {
+    let char = 'ab';
     let repCount = 5;
+
+    var result = unrepeat(char.repeat(repCount));
+
+    expect(result.repeated).toEqual(char);
+    expect(result.count).toEqual(repCount);
+  });
+
+  it('should work with no repeats', () => {
+    let char = 'test';
+    let repCount = 1;
+
+    var result = unrepeat(char.repeat(repCount));
+
+    expect(result.repeated).toEqual(char);
+    expect(result.count).toEqual(repCount);
+  });
+
+  it('should work with big number of repeats', () => {
+    let char = 'test';
+    let repCount = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23;
+
+    var result = unrepeat(char.repeat(repCount));
+
+    expect(result.repeated).toEqual(char);
+    expect(result.count).toEqual(repCount);
+  });
+
+  it('should work with empty strings', () => {
+    let char = '';
+    let repCount = 1;
 
     var result = unrepeat(char.repeat(repCount));
 
