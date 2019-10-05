2266fd56b9878f2c88575e4a9272a23ceb65ab2d
umutcanbolat <10065235+umutcanbolat@users.noreply.github.com>
2019-10-05 19:53:32 +0300

    build: add commitlint, husky, lint-staged and commitizen

--- README.md
+++ README.md
@@ -40,9 +40,15 @@ unrepeat(chorus.repeat(3));
 //=> { repeated: 'Because I\'m happy. ', count: 3 }
 ```
 
+## Conributing
+
+PR's are always welcome. There is only one thing to know before contributing. Commit messages must comply with [conventional commits](https://www.conventionalcommits.org). Otherwise [husky](https://github.com/typicode/husky) will complain. :)
+
+Thanks to [commitizen](https://github.com/commitizen), `npm run commit` command can be used to create commit messages complying with conventional commits.
+
 ## Releasing
 
-See the [RELEASING](RELEASING.md) for details.
+See [RELEASING](RELEASING.md) for details.
 
 ## Credits
 
--- src/index.ts
+++ src/index.ts
@@ -9,34 +9,39 @@ export interface Output {
  * @returns Object containing the repeated string and the repeat count.
  */
 export default function unrepeat(text: string): Output {
-  // start from the minimum prime number and iterate till the text length
+  // i: possible repeats count
+  // start from the minimum prime number and iterate till the text length.
   for (let i = 2; i <= text.length; i++) {
-    // check if text can be divided by i without remainder
+    // check if the text can be divided by i without remainder.
+    // if there is a reminder, the repeats count cannot be i. So we can skip.
     if (text.length % i === 0) {
-      // possible repeated string and its length
+      // possible repeated string and its length.
       const repLength = text.length / i;
       const repeated = text.substr(0, repLength);
 
-      // set this flag false if it is not repeated on every step
+      // set this flag false if it is not repeated on every step.
       let isRepeated = true;
 
-      // start scanning the original string
+      // start scanning the original string to find out.
+      // if possible repeated string is really repeated along the whole string.
       for (let j = 1; j < i; j++) {
         if (repeated !== text.substr(j * repLength, repLength)) {
+          // if there is a mismatch, set isRepeated to false and exit loop.
           isRepeated = false;
           break;
         }
       }
 
+      // if the possible repeated is repeated along the whole text.
       if (isRepeated) {
-        // repeated is the text itself
+        // repeat count is equal to the text length, repeated is a single char.
         if (i === text.length) {
           return {
             repeated,
             count: i,
           };
         } else {
-          // repeated is the part of the text, call unrepeat recursively
+          // repeated is the part of the text, call unrepeat again on it.
           const res = unrepeat(repeated);
           return {
             repeated: res.repeated,
